e37885bf641a18b21f9a90665c02a1c5d88a7e22
Diego Carvajal <df.carvaja@uniandes.edu.co>
2020-10-21 20:37:01 -0500

    Taller 1 TS

---
+++ tallerTypescript/scripts/dataCourses.js
@@ -0,0 +1,7 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de Sw", "Rubby Casallas", 3),
+    new Course("Sistemas Transaccionales", "Germán Bravo", 3),
+    new Course("Cálculo Vectorial", "Alexander Berenstein", 3),
+    new Course("Japonés 3", "Angélica Sensei", 3),
+];
